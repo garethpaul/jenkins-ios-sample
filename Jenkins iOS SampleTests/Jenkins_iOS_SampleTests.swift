1d771bf2a7c38eea662ab5daad3c0affb927289d
Gareth Jones <garethjones@Gareths-Mac-mini.local>
2015-05-13 11:19:13 -0700

    Initial Commit

---
+++ Jenkins iOS SampleTests/Jenkins_iOS_SampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jenkins_iOS_SampleTests.swift
+//  Jenkins iOS SampleTests
+//
+//  Created by Gareth Jones on 5/13/15.
+//  Copyright (c) 2015 Gareth Jones. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Jenkins_iOS_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
